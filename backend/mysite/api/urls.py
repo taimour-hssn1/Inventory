49ba5efae04cc2314799d65da27672ac3d7b2c33
taimour-hssn1 <bcsf22m504@pucit.edu.pk>
2025-06-25 16:12:29 +1000

    scrolling

--- backend/mysite/api/urls.py
+++ backend/mysite/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import * 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
 
 urlpatterns = [
     path('hello/', hello),
